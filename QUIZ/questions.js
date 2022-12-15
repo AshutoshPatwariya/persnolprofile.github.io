2a4fc7f5e147ead5fff355286dc1680226493b13
AshutoshPatwariya <ashutoshpatwariya2002@gmail.com>
2022-12-15 18:09:31 +0530

    quiz additon

---
+++ QUIZ/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Bay of Bengal is in which state?",
+    answer: "Liquid",
+    options: [
+      "West Bengal",
+      "Odisha",
+      "Andhra Pradesh",
+      "Liquid"
+    ]
+  },
+    {
+    numb: 2,
+    question: "If you are in a dark room with a candle, a wood stove and a gas lamp. You only have one match, so what do you light first?",
+    answer: "The match",
+    options: [
+      "Candle",
+      "wood",
+      "The match",
+      "gas lamp"
+    ]
+  },
+    {
+    numb: 3,
+    question: "I get smaller every time I take a bath. What am I?",
+    answer: "Soap",
+    options: [
+      "humans",
+      "toothbrush",
+      "Animals",
+      "Soap"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Imagine you are in a sinking rowboat surrounded by sharks. How would you survive?",
+    answer: "Stop imagining",
+    options: [
+      "fight with sharks",
+      "Pray to god to save you",
+      "Stop imagining",
+      "you would'nt survive"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is always coming, but never arrives?",
+    answer: "Tomorrow",
+    options: [
+      "Girlfriend",
+      "Boyfriend",
+      "Tomorrow",
+      "IPL cup for RCB"
+    ]
+  },
+  
+];
